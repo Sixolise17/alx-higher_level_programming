23088fe464c1bb446ed3b293652087d43bdfff9f
Sixolise Klaas <sixoliseklaas17@gmail.com>
2021-09-12 19:00:02 -0700

    BIT MANIPULATION

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx > len(my_list) -1:
-        return None
-    elif idx < 0:
-        return None
-    else:
-        return(my_list[idx])
+    return(my_list[idx] if idx < 0 or idx > len(my_list) else "None")
