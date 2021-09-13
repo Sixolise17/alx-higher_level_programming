56d5db0eb62e61db643776b2334d6580263a0ebe
Sixolise Klaas <sixoliseklaas17@gmail.com>
2021-09-12 18:46:11 -0700

    BIT MANIPULATION

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    print("{}".format(my_list[4]))
+    print("{}".format(my_list[3]))
+    print("{}".format(my_list[2]))
+    print("{}".format(my_list[1]))
+    print("{}".format(my_list[0]))
+    
