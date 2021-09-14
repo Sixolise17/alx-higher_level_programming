1a19116ad4821b2679f5c07fa50b40390d6db8f2
Sixolise Klaas <sixoliseklaas17@gmail.com>
2021-09-14 04:10:25 -0700

    BIT MANIPULATION

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,6 +2,6 @@
 
 def replace_in_list(my_list, idx, element):
     if idx > len(my_list) - 1 or idx < 0:
-        return None
+        return my_list
     my_list[idx] = element
     return(my_list)
